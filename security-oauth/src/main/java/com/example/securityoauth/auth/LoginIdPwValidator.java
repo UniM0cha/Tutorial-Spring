db514dbfffa88b1957726e345758ab0ead345eda
UniM0cha <solst_ice@naver.com>
2022-04-05 17:13:33 +0900

    0405 - 다른 스프링 시큐리티 강의를 보고 따라할 프로젝트 생성

--- security-oauth/src/main/java/com/example/securityoauth/auth/LoginIdPwValidator.java
+++ security-oauth/src/main/java/com/example/securityoauth/auth/LoginIdPwValidator.java
@@ -1,15 +1,30 @@
 package com.example.securityoauth.auth;
 
+import com.example.securityoauth.user.User;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+// id, pw에 따른 인증을 처리하기 위한 UserDetailsService 구현체
+@Service
 public class LoginIdPwValidator implements UserDetailsService {
 
+  // 로그인시에 사용자가 입력한 id가 loadUserByUsername의 인자로 들어온다.
+  // 들어온 인자로 DB에서 select 해준다.
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    // TODO Auto-generated method stub
     return null;
+  }
+
+  // 암호를 어떻게 암호화 할지 설정
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
