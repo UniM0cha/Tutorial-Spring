cd10609147113420b82130279cb034dd580107b0
UniM0cha <solst_ice@naver.com>
2022-04-06 10:10:49 +0900

    0406 - jpabook에서 오류 없도록 수정

--- jpabook/ch11-jpa-shop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpabook/ch11-jpa-shop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -24,7 +24,7 @@ public class MemberService {
      */
     public Long join(Member member) {
 
-        validateDuplicateMember(member); // 중복 회원 검증
+        validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
