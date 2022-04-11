27aa523770963163efc47742842054b06924cef6
UniM0cha <solst_ice@naver.com>
2022-04-11 17:24:03 +0900

    0411 - 인프런 강의 : 웹 계층 개발 - 홈화면과 레이아웃

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -9,12 +9,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -33,11 +36,30 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+    // 생성 메서드 대신 기본 생성자를 쓰지 못하도록 protected로 설정한다.
+    // protected OrderItem() {}
+
+    // 생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // 비즈니스 로직
+
     // 재고 수량을 원래 상태로 원복시켜준다.
     public void cancel() {
         getItem().addStock(count);
     }
 
+    // 조회 로직
+
+    // 주문 상품 전체 가격 조회
     public int getTotalPrice() {
         return getOrderPrice() * getCount();
     }
---
+++ jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,100 @@
+package jpabook.jpashop.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
+import jpabook.jpashop.repository.OrderRepository;
+
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        Member member = createMember();
+
+        Book book = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals(OrderStatus.ORDER, getOrder.getStatus(), "상품 주문시 상태는 ORDER");
+        assertEquals(1, getOrder.getOrderItems().size(), "주문한 상품 종류 수가 정확해야 한다.");
+        assertEquals(10000 * orderCount, getOrder.getTotalPrice(), "주문 가격은 가격 * 수량이다.");
+        assertEquals(8, book.getStockQuantity(), "주문 수량 만큼 재고가 줄어야 한다.");
+    }
+
+    private Book createBook(String name, int price, int stockQuantity) {
+        Book book = new Book();
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        em.persist(book);
+        return book;
+    }
+
+    private Member createMember() {
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+        return member;
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        // given
+        Member member = createMember();
+        Book book = createBook("시골 JPA", 10000, 10);
+        int orderCount = 11;
+
+        // when & then
+        assertThrows(NotEnoughStockException.class, () -> {
+            orderService.order(member.getId(), book.getId(), orderCount);
+        });
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        Member member = createMember();
+        Book book = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        orderService.cancelOrder(orderId);
+
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals(OrderStatus.CANCEL, getOrder.getStatus(), "주문 취소 시 상태는 CANCEL 이다.");
+        assertEquals(10, book.getStockQuantity(), "주문이 취소된 상품은 그만큼 재고가 증가해야 한다.");
+    }
+
+}
